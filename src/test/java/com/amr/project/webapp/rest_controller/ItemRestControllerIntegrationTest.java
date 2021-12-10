9b17869601864dfc9f40e042443d268d67677848
Konstantin Narishkin <narishun@gmail.com>
2021-12-10 10:15:43 +0300

    Tests4

--- src/test/java/com/amr/project/webapp/rest_controller/ItemRestControllerIntegrationTest.java
+++ src/test/java/com/amr/project/webapp/rest_controller/ItemRestControllerIntegrationTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ItemRestControllerITest {
+class ItemRestControllerIntegrationTest {
 
     @Autowired
     ItemService itemService;
